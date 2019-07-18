bcee5c8cb433f52900b91ef8b5bc32c0bc3df4d9
gintamamadao <gintamaginsan@126.com>
2019-07-18 10:44:38 +0800

    1.0-完善介绍

--- README.md
+++ README.md
@@ -20,94 +20,57 @@ const { Schema } = require("schema-verify");
 const schemaInfo = {
     type: Object,
     restrict: true,
-    hint: {
-        type: "type error hint text",
-        restrict: "restrict error hint text"
-    },
-    props: [
-        {
-            index: "email",
+    props: {
+        id: {
+            required: true,
+            type: Number,
+            natural: true
+        },
+        email: {
             required: true,
             type: String,
             pattern: "email",
-            length: { min: 3, max: 32 },
-            match: /abc/
+            length: { min: 3, max: 32 }
         },
-        {
-            index: "str",
+        gender: {
             required: true,
             type: String,
-            enum: ["a", "b", "c"]
+            enum: ["male", "female"]
         },
-        {
-            index: "no",
-            required: true,
-            type: Number,
-            range: { min: 0, max: 2 },
-            natural: true,
-            enum: [1, 2],
-            custom: () => true
-        },
-        {
-            index: "obj",
+        address: {
             required: true,
             type: Object,
             restrict: true,
-            props: [
-                {
-                    index: "phone",
+            props: {
+                city: {
                     required: true,
                     type: String,
-                    pattern: "phone"
-                }
-            ]
-        },
-        {
-            index: "arr",
-            required: true,
-            type: Array,
-            length: { min: 0, max: 2 },
-            elements: [
-                {
-                    index: 0,
-                    type: Object,
-                    required: true,
-                    restrict: true,
-                    props: [
-                        {
-                            index: "uri",
-                            required: true,
-                            type: String,
-                            pattern: "uri"
-                        }
-                    ]
+                    maxLength: 100
                 },
-                {
+                street: {
+                    required: true,
                     type: String,
-                    enum: ["a", "b", "c"]
+                    maxLength: 100
                 }
-            ]
+            }
         }
-    ]
+    }
 };
 
 const schema = new Schema(schemaInfo);
 
 const data = {
+    id: 1,
     email: "abc@abc.abc",
-    str: "a",
-    no: 1,
-    obj: {
-        phone: "12312345123"
-    },
-    arr: [
-        {
-            uri: "https://abc.abc"
-        },
-        "a"
-    ]
+    gender: "male",
+    address: {
+        city: "London",
+        street: "London street"
+    }
 };
+
 schema.verify(data);
+//true
 ```
 
 ### 变量说明
@@ -125,23 +88,18 @@ schemaInfo 既可以为对象，也可以为数组，当为数组时只要满足
 
 ```js
 const data = {
+    id: 1,
     email: "abc@abc.abc",
-    str: "a",
-    no: 1,
-    obj: {
-        phone: "12312345123"
-    },
-    arr: [
-        {
-            uri: 0
-        },
-        "a"
-    ]
+    gender: "male",
+    address: {
+        city: 1,
+        street: "London street"
+    }
 };
 schema.verify(data, true);
-// 第二参数为 true 时，校验不通过会抛出错误，上面的例子中data 的属性 arr 里面的第一个元素的属性 uri 校验不通过
+// 第二参数为 true 时，校验不通过会抛出错误，上面的例子中data 的属性 address 里面的 city 属性类型不正确
 
-// throw Error: 属性 arr: 第 0 项: 属性 uri: type 校验不通过, 错误信息：需要 string 类型
+// throw Error: 属性 address: 属性 city: type 校验不通过, 错误信息：需要 string 类型
 ```
 
 ## 校验规则
--- test/verify.example.test.js
+++ test/verify.example.test.js
@@ -4,114 +4,70 @@ describe("example", () => {
         const schemaInfo = {
             type: Object,
             restrict: true,
-            hint: {
-                type: "type error hint text",
-                restrict: "restrict error hint text"
-            },
-            props: [
-                {
-                    index: "email",
+            props: {
+                id: {
+                    required: true,
+                    type: Number,
+                    natural: true
+                },
+                email: {
                     required: true,
                     type: String,
                     pattern: "email",
-                    length: { min: 3, max: 32 },
-                    match: /abc/
+                    length: { min: 3, max: 32 }
                 },
-                {
-                    index: "str",
+                gender: {
                     required: true,
                     type: String,
-                    enum: ["a", "b", "c"]
+                    enum: ["male", "female"]
                 },
-                {
-                    index: "no",
-                    required: true,
-                    type: Number,
-                    range: { min: 0, max: 2 },
-                    natural: true,
-                    enum: [1, 2],
-                    custom: () => true
-                },
-                {
-                    index: "obj",
+                address: {
                     required: true,
                     type: Object,
                     restrict: true,
-                    props: [
-                        {
-                            index: "phone",
+                    props: {
+                        city: {
                             required: true,
                             type: String,
-                            pattern: "phone"
-                        }
-                    ]
-                },
-                {
-                    index: "arr",
-                    required: true,
-                    type: Array,
-                    length: { min: 0, max: 2 },
-                    elements: [
-                        {
-                            index: 0,
-                            type: Object,
-                            required: true,
-                            restrict: true,
-                            props: [
-                                {
-                                    index: "uri",
-                                    required: true,
-                                    type: String,
-                                    pattern: "uri"
-                                }
-                            ]
+                            maxLength: 100
                         },
-                        {
+                        street: {
+                            required: true,
                             type: String,
-                            enum: ["a", "b", "c"]
+                            maxLength: 100
                         }
-                    ]
+                    }
                 }
-            ]
+            }
         };
         const schema = new Schema(schemaInfo);
         expect(
             (() => {
                 const data = {
+                    id: 1,
                     email: "abc@abc.abc",
-                    str: "a",
-                    no: 1,
-                    obj: {
-                        phone: "12312345123"
-                    },
-                    arr: [
-                        {
-                            uri: "https://abc.abc"
-                        },
-                        "a"
-                    ]
+                    gender: "male",
+                    address: {
+                        city: "London",
+                        street: "London street"
+                    }
                 };
                 return schema.verify(data);
             })()
         ).toBeTruthy();
         expect(() => {
             const data = {
+                id: 1,
                 email: "abc@abc.abc",
-                str: "a",
-                no: 1,
-                obj: {
-                    phone: "12312345123"
-                },
-                arr: [
-                    {
-                        uri: 0
-                    },
-                    "a"
-                ]
+                gender: "male",
+                address: {
+                    city: 1,
+                    street: "London street"
+                }
             };
             return schema.verify(data, true);
         }).toThrowError(
-            "属性 arr: 第 0 项: 属性 uri: type 校验不通过, 错误信息：需要 string 类型"
+            "属性 address: 属性 city: type 校验不通过, 错误信息：需要 string 类型"
         );
     });
 });
