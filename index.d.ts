5b0832f895c5584b7f529fa7e239918a8cfa891a
gintamamadao <gintamaginsan@126.com>
2020-01-29 15:13:10 +0800

    1.3-添加类型声明

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,9 @@
+declare namespace Type {
+    namespace string {
+        let is: (v: any) => boolean;
+    }
+}
+
 export default class Schema {
     constructor(info: any);
     verify(data: any, throwError?: boolean, parent?: any): boolean;
